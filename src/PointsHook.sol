4fa1ba0f6e901e94826877e6b4bd7ea5d6728dae
Mouzayan Delbourgo <mouzayan@arcade.xyz>
2025-02-10 20:12:55 -0500

    feat(points-hook): update formatting

--- src/PointsHook.sol
+++ src/PointsHook.sol
@@ -14,79 +14,71 @@ import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 
 /**
-v4 uses transient storage which is only available after Ethereum's cancun
-hard fork and on Solidity versions >= 0.8.24
-
-We have a memecoin-type coin called TOKEN.
-We want to attach our hook into ETH <> TOKEN pools.
-Goal is to:
-- incentivize swappers to buy TOKEN in exchange for ETH
-- and for LPs to add liquidity to our pool
-This incentivization happens through the hook issuing a second POINTS
-token when desired actions occur.
-
-Rules:
-1. When a swap takes place which buys TOKEN for ETH - we will issue POINTS
-equivalent to 20% the amount of how much ETH was swapped in to the user
-
-2. When someone adds liquidity, we will issue POINTS equivalent to how much
-ETH they added
-
-For Case (1) - minting POINTS in case of swaps, we know we will need to use `afterSwap`
-For Case (2) - the `afterAddLiquidity` hook also gives an additional `BalanceDelta` delta input
-
-Final code by Haardik:
-https://github.com/haardikk21/points-hook
+ * v4 uses transient storage which is only available after Ethereum's cancun
+ * hard fork and on Solidity versions >= 0.8.24
+ *
+ * We have a memecoin-type coin called TOKEN.
+ * We want to attach our hook into ETH <> TOKEN pools.
+ * Goal is to:
+ * - incentivize swappers to buy TOKEN in exchange for ETH
+ * - and for LPs to add liquidity to our pool
+ * This incentivization happens through the hook issuing a second POINTS
+ * token when desired actions occur.
+ *
+ * Rules:
+ * 1. When a swap takes place which buys TOKEN for ETH - we will issue POINTS
+ * equivalent to 20% the amount of how much ETH was swapped in to the user
+ *
+ * 2. When someone adds liquidity, we will issue POINTS equivalent to how much
+ * ETH they added
+ *
+ * For Case (1) - minting POINTS in case of swaps, we know we will need to use `afterSwap`
+ * For Case (2) - the `afterAddLiquidity` hook also gives an additional `BalanceDelta` delta input
+ *
+ * Final code by Haardik:
+ * https://github.com/haardikk21/points-hook
  */
-
 contract PointsHook is BaseHook, ERC20 {
     using CurrencyLibrary for Currency;
     using BalanceDeltaLibrary for BalanceDelta;
 
-    constructor(
-        IPoolManager _manager,
-        string memory _name,
-        string memory _symbol
-    ) BaseHook(_manager) ERC20(_name, _symbol, 18) {}
+    constructor(IPoolManager _manager, string memory _name, string memory _symbol)
+        BaseHook(_manager)
+        ERC20(_name, _symbol, 18)
+    {}
 
     // hook permissions to return `true` for
-	// the two hook functions we are using
-    function getHookPermissions()
-        public
-        pure
-        override
-        returns (Hooks.Permissions memory)
-    {
-        return
-            Hooks.Permissions({
-                beforeInitialize: false,
-                afterInitialize: false,
-                beforeAddLiquidity: false,
-                beforeRemoveLiquidity: false,
-                afterAddLiquidity: true,
-                afterRemoveLiquidity: false,
-                beforeSwap: false,
-                afterSwap: true,
-                beforeDonate: false,
-                afterDonate: false,
-                beforeSwapReturnDelta: false,
-                afterSwapReturnDelta: false,
-                afterAddLiquidityReturnDelta: false,
-                afterRemoveLiquidityReturnDelta: false
-            });
+    // the two hook functions we are using
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterAddLiquidity: true,
+            afterRemoveLiquidity: false,
+            beforeSwap: false,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
     }
 
     // Make sure this is an ETH - TOKEN pool
     // Make sure this swap is to buy TOKEN in exchange for ETH
     // Mint points equal to 20% of the amount of ETH being swapped in
-	function afterSwap(
+    function afterSwap(
         address,
         PoolKey calldata key,
         IPoolManager.SwapParams calldata swapParams,
         BalanceDelta delta,
         bytes calldata hookData
     ) external override onlyPoolManager returns (bytes4, int128) {
-		// If this is not an ETH-TOKEN pool with this hook attached, ignore
+        // If this is not an ETH-TOKEN pool with this hook attached, ignore
         if (!key.currency0.isAddressZero()) return (this.afterSwap.selector, 0);
 
         // We only mint points if user is buying TOKEN with ETH
@@ -106,10 +98,10 @@ contract PointsHook is BaseHook, ERC20 {
         // mint the points
         _assignPoints(hookData, pointsForSwap);
 
-		return (this.afterSwap.selector, 0);
-	}
+        return (this.afterSwap.selector, 0);
+    }
 
-	function afterAddLiquidity(
+    function afterAddLiquidity(
         address,
         PoolKey calldata key,
         IPoolManager.ModifyLiquidityParams calldata,
